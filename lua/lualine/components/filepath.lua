c9326a0c788f21a5cc6566ef9f521f3c7cc8ec92
greister <greister@gmail.com>
2021-01-28 22:37:36 +0800

    Add reference

--- lua/lualine/components/filepath.lua
+++ lua/lualine/components/filepath.lua
@@ -1,3 +1,4 @@
+-- https://stackoverflow.com/questions/4525261/getting-relative-paths-in-vim
 local function filepath ()
     path = vim.fn.substitute(vim.fn.expand('%:p'), vim.fn.expand('%:p:h:h:h'), '..', ' ')
     return path 
